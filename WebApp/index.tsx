74b892b8049027df0e6973072b4ddb3d5cfb7611
Gabriel <62155954+GABE400@users.noreply.github.com>
2023-02-23 22:33:01 +0200

    first commit

---
+++ WebApp/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <p>Hello World!</p>
+    </div>
+  );
+};
+
+render(<App />, document.getElementById('root'));
